b65eb2934378f2be29c7826ba4a4d981610560aa
hasanciftci26 <hasanciftcii26@gmail.com>
2026-06-16 14:10:22 +0000

    Not found page and New Employee page

--- test/ui/v2/webapp/controller/App.controller.ts
+++ test/ui/v2/webapp/controller/App.controller.ts
@@ -4,5 +4,7 @@ import BaseController from "ui5/genatrix/test/ui/v2/controller/BaseController";
  * @namespace ui5.genatrix.test.ui.v2.controller
  */
 export default class App extends BaseController {
-
+    public onNavToHomepage() {
+        this.navTo("RouteHomepage");
+    }
 }
--- test/ui/v2/webapp/controller/NewEmployee.controller.ts
+++ test/ui/v2/webapp/controller/NewEmployee.controller.ts
@@ -3,7 +3,7 @@ import BaseController from "ui5/genatrix/test/ui/v2/controller/BaseController";
 /**
  * @namespace ui5.genatrix.test.ui.v2.controller
  */
-export default class Homepage extends BaseController {
+export default class NewEmployee extends BaseController {
     // ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
     // Properties
     // ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
@@ -13,7 +13,7 @@ export default class Homepage extends BaseController {
     // ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 
     public override onInit() {
-        this.attachPatternMatched("RouteHomepage", this.onRoutePatternMatched.bind(this));
+        this.attachPatternMatched("RouteNewEmployee", this.onRoutePatternMatched.bind(this));
     }
 
     // ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
@@ -21,7 +21,7 @@ export default class Homepage extends BaseController {
     // ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 
     public onCreateEmployee() {
-        this.navTo("RouteNewEmployee");
+        
     }
 
     // ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
